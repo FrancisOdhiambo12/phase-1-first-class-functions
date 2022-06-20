d1d2b15b8e221be9214328eeed7266c4aa113e3b
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-06-20 19:42:28 +0300

    First class functions

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function anotherFunction() {
+        console.log("This is a named function");
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return () => console.log('This is an anonymous function');
+}
+
+
+//First class function
